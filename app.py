614e066721adacbff44762a087da92af9e94b870
miteshdeshmukh <deshmukhmitesh44@gmail.com>
2025-08-16 16:29:09 +0530

    my second commit

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_prediction():
     baby_data_form=request.form
     baby_data_cleaned=get_cleaned_data(baby_data_form)
     baby_df=pd.DataFrame(baby_data_cleaned)
-    with open("model/model1.pkl","rb") as obj:
+    with open("model1.pkl","rb") as obj:
         model=pickle.load(obj)
     prediction=model.predict(baby_df)
     prediction=round(float(prediction),2)
