6a0255022459cca493b58d72cbe516effb9ed5f2
Mitesh284 <mitesh.deshmukh18502@sakec.ac.in>
2026-09-08 18:44:13 +0530

    Add model training notebook

--- test_app.py
+++ test_app.py
@@ -4,9 +4,7 @@ def test_hello_route_success():
     tester=app.test_client()
     response=tester.get('/hello')
     assert response.status_code==200
+    
 
-# def test_hello_route_faliure():
-#     tester=app.test_client()
-#     response=tester.get('/hello')
-#     assert response.status_code==500
+
 
